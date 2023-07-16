12c00059af7828d4720c44070764c553095cb77e
Joshua L <138939574+DataAnaylst1@users.noreply.github.com>
2023-07-16 12:52:48 -0400

    Prework homework

---
+++ week1/pre_work.py
@@ -0,0 +1,5 @@
+#def hello_name(username): # answer to question 1
+
+    print( f"Hello, {username}")
+
+#hello_name ('USERNAME')
---
+++ week1/python_102.py
@@ -0,0 +1,37 @@
+#def hello_name(username): Answer to question 1
+
+#    print( f"Hello, {username}")
+
+#hello_name ('USERNAME')
+
+#def first_odds():  Answer to question 2
+#    for x in range (1, 100+1):
+#        if x%2 == 1:
+#            print(x)
+            
+#first_odds()
+
+# a_list = [1, 3, 90, 43, 136] Answer to question 3
+# print(max(a_list))
+
+#input_year = int(input("Enter a year: ")) Answer to question 4
+#def is_leap(a_year):
+#    if (a_year%400 == 0) or (a_year%4 == 0 and a_year%100 != 0):
+#        return True
+#    else:
+#        return False
+#if is_leap(input_year):
+#    print("It is a leap year")
+#else:
+#    print("It is not a leap year")
+
+#def is_consecutive(a_list): This is the answer to question 5
+#    if len(a_list) == len(set(a_list)):
+#        return True
+#    else:
+#        return False
+#print(is_consecutive([1,2,3,4,5]))
+#print(is_consecutive([2,2,1,4,8]))
+
+
+    
