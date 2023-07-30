932e0d93f8135cf76ddec7f8dd67bdff259ee01c
Joshua L <138939574+DataAnaylst1@users.noreply.github.com>
2023-07-29 21:56:11 -0400

    Project update

--- week3/day3/test.py
+++ week3/day3/test.py
@@ -6,18 +6,28 @@
 # for numbers in list1():
 #     phone_number = "'(' +"
 
-def fib(n):
-    if( n <=1 ):
-        return n
-    else:
-        return (fib( n-1 ) + fib( n-2 ))
+# def fib(n):
+#     if( n <=1 ):
+#         return n
+#     else:
+#         return (fib( n-1 ) + fib( n-2 ))
 
-n = int(input("Enter number of terms: "))
-print("Fib sequence: ")
-for i in range(n):
-    print(fib(i))
+# n = int(input("Enter number of terms: "))
+# print("Fib sequence: ")
+# for i in range(n):
+#     print(fib(i))
 # so let's say 5 was the term
 # if 5 is less than or equal to 1, return 5
 # else
 # 5-1 + 5-2
 # 4 + 3
+
+thistuple = ("apple", "banana", "cherry")
+
+# i = 1
+# while i < len(thistuple):
+#     print(thistuple[i])
+#     i = i + 1
+
+mytuple = thistuple*2
+print(mytuple)
