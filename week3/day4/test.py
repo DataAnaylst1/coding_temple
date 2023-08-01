daf49520887db523ea972725a40981837a1c2480
Joshua L <138939574+DataAnaylst1@users.noreply.github.com>
2023-08-01 09:47:22 -0400

    Assignment 1 day 1

--- week3/day4/test.py
+++ week3/day4/test.py
@@ -1,10 +1,17 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 # def oddtimes(seq):
 #     ans = 0
 #     for element in oddtimes:
 #         ans ^= element
 #     return ans
 
-def myfunc(n):
+# def myfunc(n):
     
 
+v = np.array([2,-1])
+plt.plot([0,v(1)], [0,v(2)])
+axis([-3,3,-3,3])
